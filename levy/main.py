51cc26af1442a497b039d20580e6b369ed44e184
Nkz7doestelionato <nickolaslevy66@gmail.com>
2025-09-05 12:05:15 -0300

    Add files via upload

---
+++ levy/main.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+
+WIDTH, HEIGHT = 800,600
+
+screen = pygame.displayset_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Meu Joguinho")
+
+running = True
+ 
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.flip()
+
+pygame.quit()    
